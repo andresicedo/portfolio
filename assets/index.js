38081198f871caddc0a88c05dfa303a912da1e79
Andres Icedo <andresicedo24@gmail.com>
2021-04-12 22:02:55 -0500

    updated left screen img

--- assets/index.js
+++ assets/index.js
@@ -7,3 +7,14 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidebar").style.display = "none";
 }
+
+// let content = document.getElementById('content')
+// let scrollChange = -500
+
+// window.addEventListener('scroll', (e) => {
+//     let bgimg = document.getElementById('bgimg');
+//     let {top} = content.getBoundingClientRect();
+//     if(top - window.innerHeight < scrollChange) {
+//         bgimg.style.backgroundImage = '/assets/images/imgLeft2.JPG'
+//     }
+// })
